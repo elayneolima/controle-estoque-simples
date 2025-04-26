c96c3454b920dbbf0356e09ebf7f70924f9f7f45
elayneolima <elayne04lima@gmail.com>
2025-04-26 16:54:44 -0300

    Nova versão do programa

---
+++ ControleProduto/Produto.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization; 
+
+namespace ControleProduto
+{
+    internal class Produto
+    {
+        private string _nome;
+        private double _preco;
+        private int _quantidade; 
+
+
+        public Produto (string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        public double ValorTotalEstoque()
+        {
+            return _preco * _quantidade;
+        }
+
+        public void AdicionarProduto(int qte)
+        {
+            _quantidade += qte; 
+        }
+
+        public void RemoverProduto (int qte)
+        {
+            _quantidade -= qte;
+        }
+
+        public override string ToString()
+        {
+            return "Produto: "
+                   + _nome
+                   + "\n"
+                   + "Valor: R$ "
+                   + _preco
+                   + "\n"
+                   + "Quantidade: "
+                   + _quantidade
+                   + "\n"
+                   + "Valor Total Em Estoque: R$ "
+                   + ValorTotalEstoque().ToString("f2", CultureInfo.InvariantCulture);
+
+
+        }
+    }
+}
---
+++ ControleProduto/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using ControleProduto;
+using System.Globalization;
+
+
+/*
+//Apenas um teste para tentar acessar a prop. quantidade externamente.
+
+Produto p = new Produto("teste", 90.00, 10);
+Console.WriteLine(p._quantidade);
+*/
+
+#region entrada dados
+Console.Write("Informe o nome do produto: "); 
+string nome = Console.ReadLine();
+
+Console.Write("Informe o preço do produto: ");
+double preco = double.Parse(Console.ReadLine());
+
+Console.Write("Informe a quantidade do produto: ");
+int quantidade = int.Parse(Console.ReadLine());
+
+#endregion
+
+#region instanciacao da classe
+
+Produto p = new Produto(nome, preco, quantidade);
+
+Console.WriteLine(); 
+Console.WriteLine(p);
+Console.WriteLine();
+
+Console.WriteLine("Informe a quantidade a ser adicionada no estoque: ");
+int qtd = int.Parse(Console.ReadLine());
+p.AdicionarProduto(qtd);
+
+Console.WriteLine();
+Console.WriteLine(p);
+Console.WriteLine();
+
+Console.WriteLine("Informe a quantidade a ser removida do estoque: "); 
+qtd = int.Parse(Console.ReadLine());
+p.RemoverProduto(qtd);
+
+Console.WriteLine();
+Console.WriteLine(p);
+Console.WriteLine();
+
+#endregion 
+
